65305782bd4553e5ca0021c512c5e68dd8243dcc
Ben Davison <nimijneb.bd@gmail.com>
2023-07-21 23:33:26 +0100

    Link icon if no og Image

--- src/pages/b/[subbenditName]/posts/[postId]/index.tsx
+++ src/pages/b/[subbenditName]/posts/[postId]/index.tsx
@@ -56,17 +56,34 @@ const PostHeader: React.FC<{ postId: string }> = ({ postId }) => {
             <PostButtons postData={post} />
           </div>
         </div>
-        {post.url && post.ogImage && (
+        {post.url && (
           <Link
             href={post.url}
             className="relative w-64 overflow-hidden rounded-3xl border border-gray-400"
           >
-            <Image
-              src={post.ogImage as string}
-              alt="Preview image"
-              layout="fill"
-              objectFit={"cover"}
-            />
+            {post.ogImage ? (
+              <Image
+                src={post.ogImage as string}
+                alt="Preview image"
+                layout="fill"
+                objectFit={"cover"}
+              />
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="h-6 w-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244"
+                />
+              </svg>
+            )}
           </Link>
         )}
       </div>
