a0af5382e79bf5641cef30681634aa829c87d771
Ben Davison <nimijneb.bd@gmail.com>
2023-07-27 23:05:15 +0100

    Allow svg and added relevant security policies

--- next.config.mjs
+++ next.config.mjs
@@ -22,7 +22,10 @@ const config = {
     remotePatterns: [{
       protocol: "https",
       hostname: "**"
-    }]
+    }],
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
   output: "standalone"
 };
